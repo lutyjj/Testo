dbc9597a9290a7ef178ecc44b7f75872cb70748d
Daniil Rylski <addanik@gmail.com>
2020-07-12 19:01:49 +0300

    Design and question switching

--- app/src/main/java/by/lutyjj/testo/AnswerAdapter.kt
+++ app/src/main/java/by/lutyjj/testo/AnswerAdapter.kt
@@ -1,27 +1,32 @@
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quiz.R
 
-class AnswerAdapter(private val mAnswers: List<String>) :
-    RecyclerView.Adapter<AnswerAdapter.ViewHolder>() {
+class AnswerAdapter(private val mAnswers: List<String>) : RecyclerView.Adapter<AnswerAdapter.ViewHolder>() {
+    lateinit var selectedList: ArrayList<Int>
     inner class ViewHolder(listItemView: View) : RecyclerView.ViewHolder(listItemView) {
-        val nameTextView = itemView.findViewById<TextView>(R.id.answer_title)
+        val answerTitle: TextView = itemView.findViewById(R.id.answer_title)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnswerAdapter.ViewHolder {
         val context = parent.context
         val inflater = LayoutInflater.from(context)
-        val contactView = inflater.inflate(R.layout.item_contact, parent, false)
-        return ViewHolder(contactView)
+        val answerView = inflater.inflate(R.layout.item_answer, parent, false)
+        return ViewHolder(answerView)
     }
 
     override fun onBindViewHolder(viewHolder: AnswerAdapter.ViewHolder, position: Int) {
         val answer: String = mAnswers[position]
-        val textView = viewHolder.nameTextView
-        textView.text = answer
+        val answerTextView = viewHolder.answerTitle
+        answerTextView.text = answer
+        answerTextView.setOnClickListener{
+
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/by/lutyjj/testo/MainActivity.kt
+++ app/src/main/java/by/lutyjj/testo/MainActivity.kt
@@ -1,25 +1,93 @@
 package by.lutyjj.testo
 
 import AnswerAdapter
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.SystemClock
+import android.util.Log
 import android.view.View
+import android.view.animation.AnimationUtils
+import android.view.animation.LayoutAnimationController
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quiz.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.sql.Time
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var list: ArrayList<String>
+    private var totalQuestions: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
         val db = DatabaseHelper(this)
-        val questionIndex = 6
+        val question = findViewById<TextView>(R.id.questionTitle)
+        list = updateList(db)
+        val rvAnswers = findViewById<View>(R.id.rvAnswers) as RecyclerView
+        val adapter = AnswerAdapter(list)
+        rvAnswers.adapter = adapter
+        rvAnswers.layoutManager = LinearLayoutManager(this)
+        rvAnswers.addItemDecoration(MarginItemDecoration(
+            resources.getDimension(R.dimen.default_padding).toInt()))
 
+        startTimer()
+        setTotalQuestions()
+
+        val skipBtn = findViewById<Button>(R.id.skip)
+        skipBtn.setOnClickListener{
+            list.clear()
+            list.addAll(updateList(db))
+            val animation = AnimationUtils.loadAnimation(this, R.anim.item_animation_fall_down)
+            question.startAnimation(animation)
+            rvAnswers.scheduleLayoutAnimation()
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun setTotalQuestions() {
+        val questionCounter = findViewById<TextView>(R.id.question_counter)
+        questionCounter.text = totalQuestions.toString()
+    }
+
+    private fun startTimer() {
+        val timer = findViewById<TextView>(R.id.timer)
+        val countUpTimer = object : CountDownTimer(Long.MAX_VALUE,1000) {
+            var seconds = 0
+            var minutes = 0
+
+            override fun onFinish() {
+                seconds = 0
+                minutes = 0
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                if (seconds < 10)
+                    timer.text = "$minutes:0$seconds"
+                else
+                    timer.text = "$minutes:$seconds"
+                seconds++
+                if (seconds == 60) {
+                    seconds = 0
+                    minutes++
+                }
+            }
+        }
+        countUpTimer.start()
+    }
+
+    private fun updateList(db: DatabaseHelper): ArrayList<String> {
         val question = findViewById<TextView>(R.id.questionTitle)
         val questionCursor = db.questions
+        totalQuestions = questionCursor.count
+        val questionIndex = (1 until totalQuestions).random()
         questionCursor.moveToPosition(questionIndex - 1)
         question.text = questionCursor.getString(1)
 
@@ -33,9 +101,6 @@ class MainActivity : AppCompatActivity() {
                 correctList.add(c.getColumnIndex("is_correct"))
         } while (c.moveToNext())
 
-        val rvAnswers = findViewById<View>(R.id.rvAnswers) as RecyclerView
-        val adapter = AnswerAdapter(answerList)
-        rvAnswers.adapter = adapter
-        rvAnswers.layoutManager = LinearLayoutManager(this)
+        return answerList
     }
 }
