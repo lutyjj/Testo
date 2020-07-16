ae59e26b45b9f297109825f264388633701f7370
Daniil Rylski <addanik@gmail.com>
2020-07-16 21:17:23 +0300

    Refactor & use strings

---
+++ app/src/main/java/by/lutyjj/testo/TestActivity.kt
@@ -0,0 +1,166 @@
+package by.lutyjj.testo
+
+import AnswerAdapter
+import android.content.res.ColorStateList
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.lutyjj.testo.R
+
+class TestActivity : AppCompatActivity() {
+    private val adapter = AnswerAdapter()
+    private var totalQuestions: Int = 0
+    private var totalMistakes: Int = 0
+    private var totalCorrect: Int = 0
+    private var currentQuestion: Int = 0
+    private var isFabReady: Boolean = false
+    private var answeredQuestions: ArrayList<Int> = ArrayList()
+
+    private lateinit var db: DatabaseHelper
+    private lateinit var answerList: ArrayList<String>
+    private lateinit var correctList: ArrayList<Int>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+
+        val rvAnswers = findViewById<View>(R.id.rvAnswers) as RecyclerView
+        rvAnswers.adapter = adapter
+        rvAnswers.layoutManager = LinearLayoutManager(this)
+        rvAnswers.addItemDecoration(
+            MarginItemDecoration(
+                resources.getDimension(R.dimen.default_padding).toInt()
+            )
+        )
+
+        db = DatabaseHelper(this)
+        setTimer()
+        updateQuestion()
+
+        val skipBtn = findViewById<Button>(R.id.skip_button)
+        skipBtn.setOnClickListener { updateQuestion() }
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            if (isFabReady) {
+                fab.setImageResource(R.drawable.ic_baseline_done_outline_24)
+                fab.backgroundTintList =
+                    ColorStateList.valueOf(getColor(R.color.colorAccent))
+                resetCorrectAnswers()
+                updateQuestion()
+            } else {
+                val list = adapter.selectedList
+                list.sort()
+                highlightAnswers()
+                if (correctList == list) {
+                    isFabReady
+                    answeredQuestions.add(currentQuestion)
+                    fab.backgroundTintList =
+                        ColorStateList.valueOf(getColor(R.color.fabGreen))
+                    totalCorrect++
+                } else {
+                    fab.setImageResource(R.drawable.ic_close_outline)
+                    fab.backgroundTintList =
+                        ColorStateList.valueOf(getColor(R.color.fabRed))
+                    totalMistakes++
+                }
+            }
+            isFabReady = !isFabReady
+        }
+    }
+
+    private fun highlightAnswers() {
+        adapter.correctList = correctList
+        for (i in correctList) adapter.notifyItemChanged(i)
+    }
+
+    private fun resetCorrectAnswers() {
+        adapter.correctList.clear()
+    }
+
+    private fun setTimer() {
+        val timer = findViewById<TextView>(R.id.timer)
+        val countUpTimer = object : CountDownTimer(Long.MAX_VALUE, 1000) {
+            var seconds = 0
+            var minutes = 0
+
+            override fun onFinish() {
+                seconds = 0
+                minutes = 0
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                if (seconds < 10)
+                    timer.text = getString(R.string.timer_under_10_min, minutes, seconds)
+                else
+                    timer.text = getString(R.string.timer_over_10_min, minutes, seconds)
+                seconds++
+                if (seconds == 60) {
+                    seconds = 0
+                    minutes++
+                }
+            }
+        }
+        countUpTimer.start()
+    }
+
+    private fun updateData() {
+        val question = findViewById<TextView>(R.id.questionTitle)
+        val questionCursor = db.questions
+        totalQuestions = questionCursor.count
+        updateQuestionCounter()
+
+        var questionIndex = (1..totalQuestions).random()
+        while (answeredQuestions.contains(questionIndex)) {
+            questionIndex = (1..totalQuestions).random()
+        }
+
+        currentQuestion = questionIndex
+        questionCursor.moveToPosition(questionIndex - 1)
+        question.text = questionCursor.getString(1)
+
+        val c = db.getAnswers(questionIndex)
+
+        val answerList: ArrayList<Pair<String, Int>> = ArrayList()
+        do answerList.add(c.getString(0) to c.getInt(1))
+        while (c.moveToNext())
+
+        answerList.shuffle()
+
+        val correctList = ArrayList<Int>()
+        for ((index, answer) in answerList.unzip().second.withIndex())
+            if (answer == 1)
+                correctList.add(index)
+
+        this.answerList = answerList.unzip().first as ArrayList<String>
+        this.correctList = correctList
+    }
+
+    private fun updateQuestion() {
+        val question = findViewById<TextView>(R.id.questionTitle)
+        val mistakesTv = findViewById<TextView>(R.id.mistakes_counter)
+        val rvAnswers = findViewById<RecyclerView>(R.id.rvAnswers)
+
+        mistakesTv.text = totalMistakes.toString()
+        adapter.list.clear()
+        adapter.selectedList.clear()
+        updateData()
+        adapter.list.addAll(answerList)
+        val animation = AnimationUtils.loadAnimation(this, R.anim.item_animation)
+        question.startAnimation(animation)
+        rvAnswers.scheduleLayoutAnimation()
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun updateQuestionCounter() {
+        val questionCounter = findViewById<TextView>(R.id.question_counter)
+        questionCounter.text = getString(R.string.question_counter, totalCorrect, totalQuestions)
+    }
+}
